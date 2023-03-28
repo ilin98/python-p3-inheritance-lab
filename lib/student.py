4bb787c4f9b88a8e7ad457aae1b637b0ad1b5173
Isaac <isaac.lin.pnc@gmail.com>
2023-03-28 16:13:03 -0400

    finished lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+
+    def __init__(self, first_name, last_name, knowledge = 0):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
+    def learn(self, knowledge):
+        self.knowledge = knowledge
